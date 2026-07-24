255f9c420e8af3a147db675610d76a735a506343
iShivangi <shivangip801@gmail.com>
2026-07-24 20:39:32 +0530

    Create 122A-LuckyDivision.java

---
+++ 122A-LuckyDivision.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] luckyNum = {
+            4, 7, 
+            44, 47, 74, 77, 
+            444, 447, 474, 477, 744, 747, 774, 777
+        };
+        boolean isAlmostLucky = false;
+        for (int lucky : luckyNum) {
+            if (n % lucky == 0) {
+                isAlmostLucky = true;
+                break; 
+            }
+        }
+        
+        if (isAlmostLucky) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
